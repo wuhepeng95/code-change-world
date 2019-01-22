25537545cd834769b896f531bda327313b504202
吴合鹏 <1041259750@qq.com>
2019-01-22 11:13:33 +0800

    线程池测试：Callable有返回值，如果还没有return，get()返回值的时候会阻塞

--- web-demo/src/test/java/TestBase.java
+++ web-demo/src/test/java/TestBase.java
@@ -36,10 +36,37 @@ public class TestBase {
         Map<String,Object> param = new HashMap<>();
         param.put("pageSize",10);
         param.put("startIndex",1);
+
+        //创建现场池
         ExecutorService executorService = Executors.newCachedThreadPool();
-        Callable<Object> call1 = () -> whpTestDao.getall(param);
+
+        // Runnable run 方法返回为空，Callable call 方法返回范型V
+
+        // 原生写法
+        Callable<List<WhpTest2>> call1 = new Callable<List<WhpTest2>>() {
+            @Override
+            public List<WhpTest2> call() throws Exception {
+                // 先暂停两秒
+                System.out.println("我是"+Thread.currentThread().getName()+"，我暂停了两秒。如果想get()我的值，得先等我执行完return ^.^");
+                TimeUnit.SECONDS.sleep(2);
+                return whpTestDao.getall(param);
+            }
+        };
+
+        // lambda最简写法
         Callable<Object> call2 = () -> whpTestDao.count(param);
-        executorService.submit(call1);
-        executorService.submit(call2);
+
+        // summit后执行 返回结果
+        Future<List<WhpTest2>> submit = executorService.submit(call1);
+        Future<Object> submit1 = executorService.submit(call2);
+
+        // 输出结果
+        try {
+            System.out.println("获取查询结果集：");
+            submit.get().forEach(whpTest2 -> System.out.println(whpTest2.toString()));
+            System.out.println("获取查询结果数量："+(int)submit1.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
