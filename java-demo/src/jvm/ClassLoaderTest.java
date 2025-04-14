b88ccb625bbc9483e9154c2cf9bc414e4705218b
吴合鹏28863 <hepeng.wu@going-link.com>
2025-04-14 15:49:18 +0800

    add: 启动类加载器输出class的是null

--- java-demo/src/jvm/ClassLoaderTest.java
+++ java-demo/src/jvm/ClassLoaderTest.java
@@ -1,5 +1,7 @@
 package jvm;
 
+import org.apache.commons.lang3.AnnotationUtils;
+
 /**
  * Created by whp on 2018/10/26
  * 类加载机制：
@@ -31,6 +33,10 @@ class SingleTon {
 public class ClassLoaderTest {
 
     public static void main(String[] args) {
+        System.out.println(ClassLoaderTest.class.getClassLoader()); // sun.misc.Launcher$AppClassLoader@18b4aac2
+        System.out.println(AnnotationUtils.class.getClassLoader()); // sun.misc.Launcher$AppClassLoader@18b4aac2
+        // 启动类加载器（Bootstrap ClassLoader） 加载的，而启动类加载器是用 C++ 编写的，并且它 不是 Java 类，所以无法通过 getClassLoader() 方法获取到它的类加载器。
+        System.out.println(Math.class.getClassLoader()); // 一直是null
         SingleTon instance = SingleTon.getInstance();
         System.out.println("count1 = " + instance.count1);
         System.out.println("count2 = " + instance.count2);
